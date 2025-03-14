73301468dab6ef88adcdcf59efe61282acbd3100
mmrdi <elnkno@gmail.com>
2025-03-14 14:52:43 +0100

    feat(shared) init test

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,3 +1,11 @@
 /**
  * Shared types for React front and Node api backend
  */
+
+export type Image = {
+    _id: string
+    id: string
+    url: string
+    upvotes: number
+    participations: number
+}
