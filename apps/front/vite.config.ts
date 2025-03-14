9565cd2d65bc205e133bc68262ba750fa2f77906
mmrdi <elnkno@gmail.com>
2025-03-14 10:50:35 +0100

    feat(front) config env var

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, path.join(process.cwd(), "../../"), "VITE_")
+    const defineEnv = Object.keys(env).reduce(
+        (acc: Record<any, string>, key) => {
+            acc[`import.meta.env.${key}`] = JSON.stringify(env[key])
+            return acc
+        },
+        {}
+    )
+
+    return {
+        plugins: [react()],
+        define: defineEnv
+    }
 })
